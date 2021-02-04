6e2cf2323cbc909626483271215b5bb6139cb32f
Sushi <weingaben@gmail.com>
2021-02-04 20:06:54 +0100

    Base site setup

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,73 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="wrapper">
+    <Begin/>
+    <div class="copyright">
+      <p>Made with ❤ by Sushi⠀•⠀©2021</p>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Begin from './components/Begin.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Begin
   }
 }
 </script>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  background: #121717;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  height: 100vh;
+  width: 100vw;
+}
+
+#wrapper {
+  height: 100vh;
+  width: 100vw;
+  position: relative;
+}
+
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@200&display=swap');
+
+.copyright {
+  font-family: 'Montserrat', sans-serif;
+  color: #333;
+  position: absolute;
+  bottom: 20px;
+  left: 0;
+  right: 0;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-size: 16px;
+  transition: font-size .06s;
+}
+
+@media screen and (min-width: 768px) {
+  .copyright {
+    font-size: 22px;
+  }
+}
+
+@media screen and (min-width: 1025px) {
+  .copyright {
+    font-size: 26px;
+  }
+}
+
+@media screen and (min-width: 1921px) {
+  .copyright {
+    font-size: 30px;
+  }
 }
 </style>
---
+++ src/components/Begin.vue
@@ -0,0 +1,142 @@
+<template>
+  <div class="Begin">
+    <button @click="displayMaintenance">{{ btntext }}</button>
+    <span v-show="showError">UNDER CONSTRUCTION</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Begin',
+  data() {
+     return {
+       btntext: "Verify",
+       showError: false
+     }
+  },
+  methods: {
+    displayMaintenance () {
+      this.btntext = 'Offline'
+      this.showError = true;
+    }
+  } 
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap');
+
+.Begin {
+  height: 100vh;
+  display: grid;
+  grid-template-columns: auto;
+  grid-template-rows: 30% 60px auto;
+}
+
+@keyframes GradientFade {
+    0%{background-position:0% 50%}
+    50%{background-position:100% 50%}
+    100%{background-position:0% 50%}
+}
+
+.Begin button {
+  --btnwidth: 180px;
+  --btnheight: 60px;
+  --fontsize: 24px;
+  transition: height .06s, width .06s, font-size .06s;
+  justify-self: center;
+  align-self: center;
+  grid-row: 2;
+  font-family: 'Montserrat', sans-serif;
+  background-image: linear-gradient(to bottom right, #617de2, #54cad4);
+  background-size: 400% 400%;
+  animation: GradientFade 4s ease infinite;
+  background-color: #7289DA;
+  color: white;
+  border: 0;
+  border-radius: 100px;
+  text-align: center;
+  text-decoration: none;
+  font-size: var(--fontsize);
+  padding: 10px 28px;
+  font-weight: bold;
+  width: var(--btnwidth);
+  height: var(--btnheight);
+  outline: none;
+}
+
+.Begin button:active {
+  width: calc(var(--btnwidth) - 24px);
+  height: calc(var(--btnheight) - 8px);
+  font-size: calc(var(--fontsize) - 4px);
+}
+
+.Begin span {
+  justify-self: center;
+  align-self: top;
+  grid-row: 3;
+  font-size: 16px;
+  font-family: 'Montserrat', sans-serif;
+  color: white;
+  text-align: center;
+  padding: 10px;
+}
+
+@media screen and (min-width: 768px) {
+  .Begin {
+    grid-template-rows: 30% 80px auto;
+  }
+  
+  .Begin button {
+    --btnwidth: 240px;
+    --btnheight: 80px;
+    --fontsize: 34px;
+    width: var(--btnwidth);
+    height: var(--btnheight);
+    font-size: var(--fontsize);
+    grid-template-rows: 30% 80px auto;
+  }
+
+  .Begin span {
+    font-size: 22px;
+  }
+}
+
+@media screen and (min-width: 1025px) {
+  .Begin {
+    grid-template-rows: 30% 100px auto;
+  }
+
+  .Begin button {
+    --btnwidth: 300px;
+    --btnheight: 100px;
+    --fontsize: 44px;
+    width: var(--btnwidth);
+    height: var(--btnheight);
+    font-size: var(--fontsize);
+  }
+
+  .Begin span {
+    font-size: 28px;
+  }
+}
+
+@media screen and (min-width: 1921px) {
+  .Begin {
+    grid-template-rows: 30% 120px auto;
+  }
+
+  .Begin button {
+    --btnwidth: 360px;
+    --btnheight: 120px;
+    --fontsize: 54px;
+    width: var(--btnwidth);
+    height: var(--btnheight);
+    font-size: var(--fontsize);
+  }
+
+  .Begin span {
+    font-size: 34px;
+  }
+}
+</style>
